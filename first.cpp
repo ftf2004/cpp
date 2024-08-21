848b7f61c8c699111dd727f71b73d311f6c071c6
ftf2004 <frickthefasak@outlook.com>
2024-08-21 09:36:26 +0000

    trhrth

---
+++ first.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    // int x,y;
+    // cin >> x >> y;
+    // cout << "X: "<< x << "\n" <<"Y: "<<y;
+
+    // string str;
+    // getline(cin,str);
+    // cout << str;
+
+    // int age;
+    // cin >> age;
+    // if(age>=18){
+    //     cout<<"Major";
+    // }
+    // else{
+    //     cout<<"Minor";
+    // }
+
+    // int day;
+    // cin>>day;
+    // switch(day){
+    //     case 1:
+    //         cout<<"Mond";
+    //         break;
+    //     case 2:
+    //         cout<<"Tues";
+    //         break;
+    // }
+    // cout<<"Invalid";
+
+    // int arr[10];
+    // cin>>arr[0]>>arr[1]>>arr[2];
+
+    string s;
+    cin>>s;
+    cout<<s.size();
+    
+
+
+    return 0;
+}
